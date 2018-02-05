dd69fdb817eb4020a0563e20896d7a023f6f0514
Patrick Teivas <teivas.2011@mail.ru>
2018-02-05 19:15:48 +0200

    10 lines of code

---
+++ ConsoleApp1/Menu.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Menu
+    {
+        public void Kohv()
+        {
+
+        }
+
+        public void Toit()
+        {
+
+        }
+    }
+}
--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -11,8 +11,38 @@ namespace ConsoleApp1
         static void Main(string[] args)
         {
             Console.Write("Tere! ");
-            Console.WriteLine("Mida te soovite osta?\n1. Kohv\n2. Toit");
-            var valik = Console.ReadLine();
+            bool checkMenu = false;
+            int checkedValik;
+            while (checkMenu == false)
+            {
+                Console.WriteLine("Mida te soovite osta?\n1. Kohv\n2. Toit");
+                var valik = Console.ReadLine();
+                checkMenu = Int32.TryParse(valik, out checkedValik);
+                if (checkMenu == true)
+                {
+                    var Menu = new Menu();
+                    if (checkedValik == 1)
+                    {
+                        Menu.Kohv();
+                    }
+                    else if (checkedValik == 2)
+                    {
+                        Menu.Toit();
+                    }
+                    else
+                    {
+                        checkMenu = false;
+                        Console.Clear();
+                        Console.WriteLine("Vale valik!\n");
+                    }
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Vale valik!\n");
+                }
+            }
+            //
         }
     }
 }
